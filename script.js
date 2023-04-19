52c8755609054231a5b88a37e3cd301606c01ff1
James Wimmers <jimmyskydive@gmail.com>
2023-04-19 22:52:54 +1200

    start event listener + declare values and make string

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-const travelInput1 = ""
-const travelInput2 = ""
+const app = {
+init: () => {
+
+    document
+    .getElementById("btnGet")
+    .addEventListener("click", app.fetchWeather);
+}
+fetchWeather: (ev) => {...
+},
+};
 
 
-https://api.open-meteo.com/v1/forecast?latitude=-36.85&longitude=174.76&hourly=temperature_2m
+
+const departInput = document.getElementById("travelInitialInput");
+const arriveInput = document.getElementById("travelDestinationInput");
+
+departInputString = departInput.toString();
+arriveInputString = arriveInput.toString();
+
+
+
+const button = document.querySelector("button");
+
+
+
+
+
+
+// https://api.open-meteo.com/v1/forecast?latitude=-36.85&longitude=174.76&hourly=temperature_2m
