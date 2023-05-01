dbd65620d6fe0bb8755172a4adb024958dd7c449
James Wimmers <jimmyskydive@gmail.com>
2023-05-01 20:36:28 +1200

    slight edit

--- src/script.js
+++ src/script.js
@@ -1,13 +1,12 @@
 
-addEventListener("input", (event) => {});
-oninput = (event) => {
-    const input = document.querySelector("input");
-    const log = document.getElementById("travelInitialInput", "travelDestinationInput");
+const travelInitialInput = document.querySelector("travelInitialInput");
+const travelDestinationInput = document.querySelector("travelDestinationInput");
+const travelInitiallog = document.getElementById("travelInitialInput");
+const travelDestinationlog = document.getElementById("travelDestinationInput");
     input.addEventListener("input", updateValue)
     function updateValue(e) {
   log.textContent = e.target.value; //Retrieve typing event
 }
-};
 const btnGet = document.querySelector("#btnGet") //Click event listener - destination to use for weather api
 departInput.addEventListener()
 btnGet.addEventListener("click", e => {
@@ -16,5 +15,9 @@ btnGet.addEventListener("click", e => {
 
 console.log(WEATHER_API_KEY);
 //let button = document.querySelector("button"); // returns first element of specified selector
+
 //http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={WEATHER_API_KEY}"
 //https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={WEATHER_API_KEY}
+
+//addEventListener("input", () => {});
+//oninput = (event) => {
